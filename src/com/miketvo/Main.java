d033e87a74133e3941d8b74859a786a359bccd08
miketvo <ikxplicitie@gmail.com>
2022-10-20 15:19:25 +0700

    Reindent files with 4 spaces

--- src/com/miketvo/Main.java
+++ src/com/miketvo/Main.java
@@ -2,20 +2,20 @@ package com.miketvo;
 
 public class Main {
 
-  public static void main(String[] args) {
-    int iterations = 1000;
-    long startTime = System.nanoTime();
-    for (int i = 0; i < iterations; i++) {
-      System.out.printf("\nIteration %d\n", i);
-      for (int n = 1; n < 542; n++) {
-        System.out.printf("n=%d; found %d primes\n", n, PrimeSieve.sieve(n).length);
-      }
-    }
-    long endTime = System.nanoTime();
-    long totalTimeElapsed = endTime - startTime;
-    long averageIterationDuration = totalTimeElapsed / iterations;
+    public static void main(String[] args) {
+        int iterations = 1000;
+        long startTime = System.nanoTime();
+        for (int i = 0; i < iterations; i++) {
+            System.out.printf("\nIteration %d\n", i);
+            for (int n = 1; n < 542; n++) {
+                System.out.printf("n=%d; found %d primes\n", n, PrimeSieve.sieve(n).length);
+            }
+        }
+        long endTime = System.nanoTime();
+        long totalTimeElapsed = endTime - startTime;
+        long averageIterationDuration = totalTimeElapsed / iterations;
 
-    System.out.printf("\nElapsed: %dns | Iteration Avg: %dns\n", totalTimeElapsed, averageIterationDuration);
-    System.out.printf("Elapsed: %dms | Iteration Avg: %dms\n", totalTimeElapsed / 1000000, averageIterationDuration / 1000000);
-  }
+        System.out.printf("\nElapsed: %dns | Iteration Avg: %dns\n", totalTimeElapsed, averageIterationDuration);
+        System.out.printf("Elapsed: %dms | Iteration Avg: %dms\n", totalTimeElapsed / 1000000, averageIterationDuration / 1000000);
+    }
 }
--- src/com/miketvo/PrimeSieve.java
+++ src/com/miketvo/PrimeSieve.java
@@ -4,64 +4,64 @@ import java.util.Arrays;
 
 public class PrimeSieve {
 
-  public static boolean isPrime(int n) {
-    if (n < 2) {
-      return false;
-    } else {
-      for (int i = 2; i < Math.round(Math.sqrt(n)); i++) {
-        if (n % i == 0) {
-          return false;
+    public static boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        } else {
+            for (int i = 2; i < Math.round(Math.sqrt(n)); i++) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+            return true;
         }
-      }
-      return true;
-    }
-  }
-
-  public static boolean checkPrimes(int[] n) {
-    for (int i: n) {
-      if (!isPrime(i)) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  public static int[] sieve(int n) {
-    boolean[] isPrime = new boolean[n];
-    Arrays.fill(isPrime, Boolean.TRUE);
-    isPrime[0] = false;
-
-    int p = 2;
-    while (p - 1 < Math.round(Math.sqrt(isPrime.length))) {
-      int multiplier = p;
-      int notPrime = p * multiplier;
-      while (notPrime <= n) {
-        isPrime[notPrime - 1] = false;
-        multiplier++;
-        notPrime = p * multiplier;
-      }
-
-      p++;
-      while (p - 1 < isPrime.length && !isPrime[p - 1]) {
-        p++;
-      }
     }
 
-    int primeCount = 0;
-    for (boolean isPrimeBool: isPrime) {
-      if (isPrimeBool) {
-        primeCount++;
-      }
+    public static boolean checkPrimes(int[] n) {
+        for (int i: n) {
+            if (!isPrime(i)) {
+                return false;
+            }
+        }
+        return true;
     }
 
-    int[] primes = new int[primeCount];
-    int j = 0;
-    for (int i = 1; i < isPrime.length; i++) {
-      if (isPrime[i]) {
-        primes[j++] = i + 1;
-      }
-    }
+    public static int[] sieve(int n) {
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime, Boolean.TRUE);
+        isPrime[0] = false;
 
-    return primes;
-  }
+        int p = 2;
+        while (p - 1 < Math.round(Math.sqrt(isPrime.length))) {
+            int multiplier = p;
+            int notPrime = p * multiplier;
+            while (notPrime <= n) {
+                isPrime[notPrime - 1] = false;
+                multiplier++;
+                notPrime = p * multiplier;
+            }
+
+            p++;
+            while (p - 1 < isPrime.length && !isPrime[p - 1]) {
+                p++;
+            }
+        }
+
+        int primeCount = 0;
+        for (boolean isPrimeBool: isPrime) {
+            if (isPrimeBool) {
+                primeCount++;
+            }
+        }
+
+        int[] primes = new int[primeCount];
+        int j = 0;
+        for (int i = 1; i < isPrime.length; i++) {
+            if (isPrime[i]) {
+                primes[j++] = i + 1;
+            }
+        }
+
+        return primes;
+    }
 }
